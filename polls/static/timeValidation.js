d45e59b6f076fe811c676562004f35b109d1386e
Maryam <lotfian.maryam@gmail.com>
2020-04-20 17:08:44 +0200

    Add months to date filter

--- polls/static/timeValidation.js
+++ polls/static/timeValidation.js
@@ -6,6 +6,10 @@ $(function() {
         var month = parseInt((selected.split('-')[1]),10)
         var sName = $('#species').val();
 
+
+        const months = {'1':'January', '2':'February', '3':'March','4':'April',
+      '5':'May','6':'June', '7':'July', '8':'August', '9':'September','10':'October',
+    '11':'November','12':'December'};
       // call biosentier API
       var settings = {
         "url": "https://biosentiers.heig-vd.ch/api/species?only=periodStart&only=periodEnd&only=commonName.fr&theme=bird",
@@ -19,7 +23,7 @@ $(function() {
           const startDate = filterName.map(pS => pS.periodStart);
           const endDate = filterName.map(pE => pE.periodEnd);
         if(month<startDate[0] || month>endDate[0]){
-          alert(`${sName} seems to be visible with the period of ${startDate[0]} to ${endDate[0]}. Could you please make sure that the species name or the date is inserted correctly? You can insert the information in any case, rare cases can happen!`)
+          alert(`${sName} seems to be visible in the period of ${months[startDate[0]]} to ${months[endDate[0]]}. Could you please make sure that the species name or the date is inserted correctly? You can insert the information in any case, rare cases can happen!`)
         }
         else {
           console.log("within the period");
