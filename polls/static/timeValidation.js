cf6e29a16b146ba68023f559263724da83d08790
Maryam <lotfian.maryam@gmail.com>
2020-04-21 11:36:04 +0200

    Modify date filter

--- polls/static/timeValidation.js
+++ polls/static/timeValidation.js
@@ -1,3 +1,7 @@
+const months = {'1':'January', '2':'February', '3':'March','4':'April',
+'5':'May','6':'June', '7':'July', '8':'August', '9':'September','10':'October',
+'11':'November','12':'December'};
+
 $(function() {
     $("#datepicker").on("change",function(){
 
@@ -6,10 +10,6 @@ $(function() {
         var month = parseInt((selected.split('-')[1]),10)
         var sName = $('#species').val();
 
-
-        const months = {'1':'January', '2':'February', '3':'March','4':'April',
-      '5':'May','6':'June', '7':'July', '8':'August', '9':'September','10':'October',
-    '11':'November','12':'December'};
       // call biosentier API
       var settings = {
         "url": "https://biosentiers.heig-vd.ch/api/species?only=periodStart&only=periodEnd&only=commonName.fr&theme=bird",
@@ -17,17 +17,29 @@ $(function() {
         "timeout": 0,
       };
 
+      // make the comparison with the API response and give the feedback
       $.ajax(settings).done(function (response) {
         const filterName = response.filter(
           name => name.commonName.fr === sName);
-          const startDate = filterName.map(pS => pS.periodStart);
-          const endDate = filterName.map(pE => pE.periodEnd);
-        if(month<startDate[0] || month>endDate[0]){
-          alert(`${sName} seems to be visible in the period of ${months[startDate[0]]} to ${months[endDate[0]]}. Could you please make sure that the species name or the date is inserted correctly? You can insert the information in any case, rare cases can happen!`)
-        }
-        else {
-          console.log("within the period");
-        }
+          console.log(filterName);
+          if(filterName.length == 1){
+            const startDate = filterName[0].periodStart;
+            const endDate = filterName[0].periodEnd;
+            // const startDate = filterName.map(pS => pS.periodStart);
+            // const endDate = filterName.map(pE => pE.periodEnd);
+          if(month<startDate || month>endDate){
+            alert(`${sName} seems to be visible in the period of ${months[startDate]} to ${months[endDate]}.
+          Could you please make sure that the species name or the date is inserted correctly?
+          You can insert the information in any case, rare cases can happen!`)
+          }
+          else {
+            console.log("Input date is within the visibilty period");
+          }
+          }
+          else {
+            console.log("API has changed!");
+          }
+
       });
 
     });
