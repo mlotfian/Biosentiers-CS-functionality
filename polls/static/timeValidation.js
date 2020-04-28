5012d8e3577f0da9bc536c15c0e37f01f109d3f3
Maryam <lotfian.maryam@gmail.com>
2020-04-28 18:08:04 +0200

    Dropup date field using kendo datepicker

--- polls/static/timeValidation.js
+++ polls/static/timeValidation.js
@@ -2,12 +2,13 @@ const months = {'1':'January', '2':'February', '3':'March','4':'April',
 '5':'May','6':'June', '7':'July', '8':'August', '9':'September','10':'October',
 '11':'November','12':'December'};
 
+
 $(function() {
     $("#datepicker").on("change",function(){
 
       // get the user values
         var selected = $(this).val();
-        var month = parseInt((selected.split('-')[1]),10)
+        var month = parseInt((selected.split('-')[0]),10)
         var sName = $('#species').val();
 
       // call biosentier API
